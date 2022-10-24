d41d597a225e0f5b07e66dbbe01a636e6f5ff8fa
bintangyandiii <acutyp9@gmail.com>
2022-10-24 14:43:25 +0700

    menyelesaikan video 5(model)

---
+++ prakweb_a_203040037_laravel/app/Models/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+
+class Post 
+{
+    private static $blog_post = [
+        [
+            "title"=> "Judul Post Pertama",
+            "slug"=>"Judul Post Pertama",
+            "autor"=> "Bintang Yandi Putra",
+            "Body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis ullam quaerat iure harum officia mollitia porro itaque illum esse ut laboriosam ipsa in suscipit, voluptate inventore officiis blanditiis vero laborum obcaecati sed doloribus. Consectetur impedit facere, nobis quos molestiae sint. Ab nesciunt atque dolor, alias aut laboriosam earum ipsum libero, veniam eligendi sint illo sapiente nobis voluptatibus praesentium cupiditate autem mollitia in quae dolores exercitationem. Nemo unde provident tempore expedita, et eaque quo recusandae voluptatum placeat pariatur tempora aliquam possimus modi, minima, quibusdam dolorum eius nostrum odit amet est sapiente. Veniam libero necessitatibus hic dolores recusandae. Soluta reprehenderit perferendis veniam?"
+        ],
+        [
+            "title"=> "Judul Post Kedua",
+            "slug"=>"Judul Post Kedua",
+            "autor"=> "Bintang Yandi Putra",
+            "Body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis ullam quaerat iure harum officia mollitia porro itaque illum esse ut laboriosam ipsa in suscipit, voluptate inventore officiis blanditiis vero laborum obcaecati sed doloribus. Consectetur impedit facere, nobis quos molestiae sint. Ab nesciunt atque dolor, alias aut laboriosam earum ipsum libero, veniam eligendi sint illo sapiente nobis voluptatibus praesentium cupiditate autem mollitia in quae dolores exercitationem. Nemo unde provident tempore expedita, et eaque quo recusandae voluptatum placeat pariatur tempora aliquam possimus modi, minima, quibusdam dolorum eius nostrum odit amet est sapiente. Veniam libero necessitatibus hic dolores recusandae. Soluta reprehenderit perferendis veniam?"
+        ]
+    ];
+
+    public static function all()
+    {
+        return collect(self::$blog_post);
+    }
+
+    public static function find($slug)
+    {
+        $Posts = static::all();
+        return $Posts->firstWhere('slug', $slug);
+    }
+}
