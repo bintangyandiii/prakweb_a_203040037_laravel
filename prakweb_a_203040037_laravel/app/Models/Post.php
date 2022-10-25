218e7e3183b6b058bc56e25f72a1520d5e983a5d
bintangyandiii <acutyp9@gmail.com>
2022-10-25 19:23:32 +0700

    menyelesaikan video 7(post model)

--- prakweb_a_203040037_laravel/app/Models/Post.php
+++ prakweb_a_203040037_laravel/app/Models/Post.php
@@ -2,32 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post 
+class Post extends Model
 {
-    private static $blog_post = [
-        [
-            "title"=> "Judul Post Pertama",
-            "slug"=>"Judul Post Pertama",
-            "autor"=> "Bintang Yandi Putra",
-            "Body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis ullam quaerat iure harum officia mollitia porro itaque illum esse ut laboriosam ipsa in suscipit, voluptate inventore officiis blanditiis vero laborum obcaecati sed doloribus. Consectetur impedit facere, nobis quos molestiae sint. Ab nesciunt atque dolor, alias aut laboriosam earum ipsum libero, veniam eligendi sint illo sapiente nobis voluptatibus praesentium cupiditate autem mollitia in quae dolores exercitationem. Nemo unde provident tempore expedita, et eaque quo recusandae voluptatum placeat pariatur tempora aliquam possimus modi, minima, quibusdam dolorum eius nostrum odit amet est sapiente. Veniam libero necessitatibus hic dolores recusandae. Soluta reprehenderit perferendis veniam?"
-        ],
-        [
-            "title"=> "Judul Post Kedua",
-            "slug"=>"Judul Post Kedua",
-            "autor"=> "Bintang Yandi Putra",
-            "Body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis ullam quaerat iure harum officia mollitia porro itaque illum esse ut laboriosam ipsa in suscipit, voluptate inventore officiis blanditiis vero laborum obcaecati sed doloribus. Consectetur impedit facere, nobis quos molestiae sint. Ab nesciunt atque dolor, alias aut laboriosam earum ipsum libero, veniam eligendi sint illo sapiente nobis voluptatibus praesentium cupiditate autem mollitia in quae dolores exercitationem. Nemo unde provident tempore expedita, et eaque quo recusandae voluptatum placeat pariatur tempora aliquam possimus modi, minima, quibusdam dolorum eius nostrum odit amet est sapiente. Veniam libero necessitatibus hic dolores recusandae. Soluta reprehenderit perferendis veniam?"
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_post);
-    }
-
-    public static function find($slug)
-    {
-        $Posts = static::all();
-        return $Posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable=['title', 'excerpt', 'body'];
+    protected $guarded= ['id'];
 }
